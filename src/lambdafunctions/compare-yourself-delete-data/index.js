106715278c7df72fc2214c8da80110aa1bba7d5d
Jon Newton <newt@hey.com>
2020-12-30 12:33:35 -0500

    Update for delete lambda

--- src/lambdafunctions/compare-yourself-delete-data/index.js
+++ src/lambdafunctions/compare-yourself-delete-data/index.js
@@ -1,8 +1,46 @@
+const AWS = require('aws-sdk');
+
+//define outside of handler function 
+//to give a chance for caching the code between lambda invocations
+const dynamodb = new AWS.DynamoDB({
+  apiVersion: '2012-08-10',
+  region: 'us-east-2'
+});
+
 exports.handler = async (event) => {
-    return "Deleted!";
-    // const response = {
-    //     statusCode: 200,
-    //     body: JSON.stringify('Deleted!'),
-    // };
-    // return response;
+  const params = {
+    TableName: 'compare-yourself',
+    Key: {
+      'Userid': {
+        S: 'user_0.0025010169283241623'
+      }
+    }
+  }
+  
+  try {
+    console.log('creating promise');
+    const promise = new Promise((resolve, reject) => {
+      console.log('delete...');
+      dynamodb.deleteItem(params, (err, data) => {
+        console.log('delete done');
+        if (err) {
+          console.log('delete returned error', err);
+          reject(err)
+        }
+        else {
+          console.log('delete returned data', JSON.stringify(data));
+          resolve(data);
+        }
+      });
+    });
+  
+    const result = await promise;
+    return JSON.stringify(result);
+  }
+  catch (err) { 
+    console.log('caught error', err);
+    return {
+      error: err
+    }
+  }
 };
