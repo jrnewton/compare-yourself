5c05d66f1764a6fa8301f7f6e3b9134634ced4de
Jon Newton <newt@hey.com>
2021-01-01 22:33:16 -0500

    DELETE is now working

--- src/lambdafunctions/compare-yourself-delete-data/index.js
+++ src/lambdafunctions/compare-yourself-delete-data/index.js
@@ -7,12 +7,40 @@ const dynamodb = new AWS.DynamoDB({
   region: 'us-east-2'
 });
 
+const cisp = new AWS.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' });
+
+const getUserId = (event) => {
+  return new Promise((resolve, reject) => {
+    console.log('getUserId for token', event.accessToken);
+    
+    const cispParams = {
+      "AccessToken": event.accessToken //via body mapping template
+    };
+    
+    cisp.getUser(cispParams, (err, result) => {
+      if (err) {
+        console.log('failed on cisp.getUser', err);
+        reject(err);
+      }
+      else {
+        console.log('success on cisp.getUser', result);
+        //userid is always index zero.  See docs.
+        const userId = result.UserAttributes[0].Value;
+        console.log('userid is', userId);
+        resolve(userId);
+      }
+    });  
+  });
+}
+
 exports.handler = async (event) => {
+  const userId = await getUserId(event);
+  
   const params = {
     TableName: 'compare-yourself',
     Key: {
       'Userid': {
-        S: 'user_0.2914122110187596'
+        S: '' + userId
       }
     }
   }
